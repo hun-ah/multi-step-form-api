5da95211193896420e092844d2ce5cb2239f9546
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-27 18:06:06 -0400

    created createUser function

--- controllers/userInfo.js
+++ controllers/userInfo.js
@@ -1,8 +1,14 @@
 const UserInfo = require('../models/UserInfo');
 
 module.exports = {
-  createUser: (req, res) => {
-    const userInfo = new UserInfo(req.body);
-    console.log(userInfo);
+  createUser: async (req, res) => {
+    try {
+      const userInfo = new UserInfo(req.body);
+      await userInfo.save();
+      res.status(201).json({ message: 'User created' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    }
   },
 };

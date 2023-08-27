d6881c5bd387d329d9278b3fe0c2dd4c6317f050
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-27 18:24:57 -0400

    updated cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 5000;
 const userInfoRoute = require('./routes/userInfo');
 
 // Middleware...
-app.use(cors);
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
